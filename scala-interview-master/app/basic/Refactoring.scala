3daec21922aa4a35e94dc7e956cc7e7fd6cb97fb
Makina Abderrahim <80503497+AbderrahimMakina@users.noreply.github.com>
2021-10-20 21:17:22 +0100

    Update Refactoring.scala

--- scala-interview-master/app/basic/Refactoring.scala
+++ scala-interview-master/app/basic/Refactoring.scala
@@ -2,7 +2,8 @@ package com.particeep.test.basic
 
 /**
  * What is the complexity of the function ?
- *
+ * the complexity of this function is O(n)
+ *i will use recursivity 
  * Refactor it to be a better function
  */
 object Refactoring {
@@ -25,4 +26,51 @@ object Refactoring {
 
     return categories
   }
+  
+  
+  ////////////answer //////////////////////////
+  
+  import scala.collection.mutable.ListMap
+
+object Refactoring {
+     case class File(
+    name:     String,
+    category: String
+  )
+  
+
+ 
+
+def getCategories(files: List[File]): List[String] = {
+    val categories: List[String] = List()
+
+    
+        
+        
+        
+         if(files.isEmpty){
+    return null
+  }else{
+      if(!files.head.category.isEmpty && !categories.contains(files.head.category)) {
+          categories :+ files.head.category
+          return categories
+        }
+        
+         return  getCategories(files.tail)
+      }
+        
+    }
+
+      
+     def main(args: Array[String])
+    {
+        var file=File("abderrahim","person")
+        var files:List[File]=List(file)
+       var l:  List[String] = List()
+      l= getCategories(files)
+       println(l)
+    
+
+}
+}
 }
