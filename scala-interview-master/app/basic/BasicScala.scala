182d786a728c45af4f645d2f9d8352a9715f8cb0
Makina Abderrahim <80503497+AbderrahimMakina@users.noreply.github.com>
2021-10-20 21:12:58 +0100

    Update BasicScala.scala

--- scala-interview-master/app/basic/BasicScala.scala
+++ scala-interview-master/app/basic/BasicScala.scala
@@ -5,6 +5,12 @@ package com.particeep.test.basic
  */
 object BasicScala {
 
+   
+  /**
+ * This is basic language questions so don't use external library or build in function
+ */
+object BasicScala {
+
   /**
    * Encode parameter in url format
    *
@@ -16,12 +22,38 @@ object BasicScala {
    * input  : Map()
    * output : ""
    */
-  def encodeParamsInUrl(params: Map[String, String]): String = ???
+  def encodeParamsInUrl(params: ListMap[String, String]): String = {
+    
+       // Creating ListMap
+       
+        var listMap = ListMap("C"->"Csharp", "S"->"Scala", "J"->"Java")
+         var s="?"
+        // Iterating elements
+        
+            if(listMap.isEmpty) 
+           { println("liste est vide")
+              return "liste est vide" 
+           }else{  
+            listMap.foreach{case (key, value) => s+= key + " -> " + value+"&"}
+return (s.dropRight(1))
 
+}
+
+}
   /**
    * Test if a String is an email
    */
-  def isEmail(maybeEmail: String): Boolean = ???
+  def isEmail(maybeEmail: String): Boolean = {
+  
+
+   if("""(?=[^\s]+)(?=(\w+)@([\w\.]+))""".r.findFirstIn(maybeEmail) == None)false else true
+
+    
+    
+  }
+  
+  
+  
 
   /**
    * Compute i ^ n
@@ -34,6 +66,30 @@ object BasicScala {
    * input : (i = 99, n = 38997)
    * output : 1723793299
    */
-  def power(i: Int, n: Int): Int = ???
+  //def power(i: Int, n: Int): Int = {
+
+    
+    def power(base: Int, exp: Int): BigInt = {
+    def _power(result: BigInt, exp: Int): BigInt = exp match {
+        case 0 => 1
+        case 1 => result
+        case _ => _power(result*base, exp-1)
+    }
+    _power(base, exp)
+}
+  //////////////////test  ///////////////////////////////////////////////////////////  
+
+    
+      def main(args: Array[String]){
+     
+       encodeParamsInUrl (ListMap("C"->"Csharp", "S"->"Scala", "J"->"Java"))
+        
+     var s="abderrahim@g.tn"
+        print (isEmail(s))
+        
+        
+        println(power(2,9))
+   
+   }
 
 }
