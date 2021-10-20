f2edad26b0f849931a8c48298f8203a7fc77c81b
Makina Abderrahim <80503497+AbderrahimMakina@users.noreply.github.com>
2021-10-20 21:14:41 +0100

    Update ComputeAvarage.scala

--- scala-interview-master/app/basic/ComputeAvarage.scala
+++ scala-interview-master/app/basic/ComputeAvarage.scala
@@ -1,12 +1,27 @@
 package com.particeep.test.basic
 
-/**
+ 
+  /**
  * Compute the avarage of the list
  *
  * ex : [1, 10, 16] -> 9
  */
-object ComputeAvarage {
-
-  def average(l: List[Double]) = ???
-
+ // def average(l: List[Double]) = {
+    
+    
+def getSumAverage(numbers:List[Double]) = {
+  val sum = numbers.sum
+  val average = (sum:Double) / numbers.length
+  (average)
 }
+
+ 
+    
+     def main(args: Array[String])
+    {
+      val numbers = List(1.0, 2.0, 30.0, 45.0, 13.0)
+val average = getSumAverage(numbers)
+println("Average is:" + average)
+    }
+    
+  
